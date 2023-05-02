58cb1d9b53d6089b0887a188b00bd504e3a5b22f
moo-ongsa <ongsa.sungkanit@gmail.com>
2023-05-02 17:19:24 +0700

    tracker logger

--- src/analytics.js
+++ src/analytics.js
@@ -1,11 +1,23 @@
 import Analytics from "analytics";
 import googleTagManager from "@analytics/google-tag-manager";
 
+const logger = {
+  name: "logger",
+  page: ({ payload }) => {
+    console.log("page view fired", payload);
+  },
+  track: ({ payload }) => {
+    console.log("tracker payload", payload);
+  },
+  // Custom functions to expose to analytics instance
+};
+
 export const tracker = Analytics({
   app: "wisible-app",
   plugins: [
     googleTagManager({
       containerId: "GTM-WLBTDBW",
     }),
+    logger,
   ],
 });
